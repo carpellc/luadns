8ec536c351c0e078ee023e0212578d4b9ff04fd4
Damon Vincent <damon@ourcomputershop.com>
2026-03-26 10:32:08 -0500

    Rename pobox_app to fastmail_app and update MX records

--- templates/pobox.lua
+++ templates/pobox.lua
@@ -1,9 +1,6 @@
- function pobox_app(domain)
+ function fastmail_app(domain)
   -- Configure mail exchangers for pobox
   -- https://www.pobox.help/hc/en-us/articles/360058663514-Pobox-s-MX-records
-  mx(domain, "mx-1.pobox.com", 10)
-  mx(domain, "mx-2.pobox.com", 10)
-  mx(domain, "mx-3.pobox.com", 10)
-  mx(domain, "mx-4.pobox.com", 10)
-  mx(domain, "mx-5.pobox.com", 10)
+  mx(domain, "in1-smtp.messagingengine.com", 10)
+  mx(domain, "in2-smtp.messagingengine.com", 20)
 end
