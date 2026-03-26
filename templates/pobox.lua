4ebe7c88a619ebbccc76c49d51b7dff0c56e485f
Damon Vincent <damon@ourcomputershop.com>
2026-03-26 11:27:01 -0500

    Remove a records and copy lua to seizethechains

--- templates/pobox.lua
+++
@@ -1,6 +0,0 @@
- function fastmail_app(domain)
-  -- Configure mail exchangers for pobox
-  -- https://www.pobox.help/hc/en-us/articles/360058663514-Pobox-s-MX-records
-  mx(domain, "in1-smtp.messagingengine.com", 10)
-  mx(domain, "in2-smtp.messagingengine.com", 20)
-end
