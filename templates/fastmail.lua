eca55f48fc2a058274f8374151baebeb40b76890
Damon Vincent <damon@ourcomputershop.com>
2026-03-26 11:43:00 -0500

    Update mail exchanger configuration for fastmail

--- templates/fastmail.lua
+++ templates/fastmail.lua
@@ -1,6 +1,5 @@
  function fastmail_app(domain)
-  -- Configure mail exchangers for pobox
-  -- https://www.pobox.help/hc/en-us/articles/360058663514-Pobox-s-MX-records
+  -- Configure mail exchangers for fastmail
   mx(domain, "in1-smtp.messagingengine.com", 10)
   mx(domain, "in2-smtp.messagingengine.com", 20)
 end
